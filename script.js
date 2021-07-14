e7769ec3ce5ab745744a60c9608ff1722eb5a170
AkersA <85839016+AkersA@users.noreply.github.com>
2021-07-14 18:13:20 -0400

    cleaned up fromat

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 (function (){
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  for (var i = 0; i > names.length; i++){
+  for (var i = 0; i < names.length; i++){
     var firstLetter = names[i].toLowerCase().charAt(0);
-  if (firstLetter == "j") {
-      byeSpeaker.speak(names[i]);
-  } else {
-      helloSpeaker.speak(names[i]);
-  }
-  }
+      if (firstLetter === "j") {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
 })();
